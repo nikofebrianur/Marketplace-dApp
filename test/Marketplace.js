67864f9a53d5773fd7a48498bb26592e5504777b
nikofebrianur <nikofebrianur@gmail.com>
2023-05-29 21:11:18 +0800

    test for deployment

--- test/Marketplace.js
+++ test/Marketplace.js
@@ -5,5 +5,21 @@ const tokens = (n) => {
 }
 
 describe("Marketplace", () => {
+  let marketplace
+  let deployer, buyer
+
+  beforeEach(async () => {
+    [deployer, buyer] = await ethers.getSigners();
+    console.log(deployer, buyer);
+
+    const Marketplace = await ethers.getContractFactory("Marketplace");
+    marketplace = await Marketplace.deploy();
+  })
+
+  describe("Deployment", () => {
+    it("Sets the owner", async () => {
+      expect(await marketplace.owner()).to.equal(deployer.address);
+    })
+  })
 
 })
