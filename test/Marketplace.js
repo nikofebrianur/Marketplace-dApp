8dde6ba1fd2552426b3fdd905dcc7cee0b1ff4fe
nikofebrianur <nikofebrianur@gmail.com>
2023-05-29 21:17:00 +0800

    Listing test

--- test/Marketplace.js
+++ test/Marketplace.js
@@ -1,12 +1,12 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+};
 
 describe("Marketplace", () => {
-  let marketplace
-  let deployer, buyer
+  let marketplace;
+  let deployer, buyer;
 
   beforeEach(async () => {
     [deployer, buyer] = await ethers.getSigners();
@@ -14,12 +14,34 @@ describe("Marketplace", () => {
 
     const Marketplace = await ethers.getContractFactory("Marketplace");
     marketplace = await Marketplace.deploy();
-  })
+  });
 
   describe("Deployment", () => {
     it("Sets the owner", async () => {
-      expect(await marketplace.owner()).to.equal(deployer.address);
+      expect(await marketplace.owner()).to.equal(deployer.address)
+    });
+  });
+
+  describe("Listing", () => {
+    let transaction;
+
+    beforeEach(async () => {
+      transaction = await marketplace.connect(deployer).listProducts(
+        1,
+        "Book",
+        "Crime",
+        "IMAGE",
+        100,
+        5,
+        10
+      );
+      await transaction.wait();
     })
-  })
+
+    it("Returns item attributes", async () => {
+      const item = await marketplace.items(1);
+      expect(item.id).to.equal(1);
+    });
+  });
 
 })
