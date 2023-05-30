05f0f51831dbeb56c9137d98fa2fc912950702dc
nikofebrianur <nikofebrianur@gmail.com>
2023-05-30 21:02:18 +0800

    deploy configuration

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,3 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat")
 const { items } = require("../src/items.json")
 
@@ -12,7 +6,13 @@ const tokens = (n) => {
 }
 
 async function main() {
+  const [deployer] = await ethers.getSigners()
 
+  const Marketplace = await hre.ethers.getContractFactory("Marketplace")
+  const marketplace = await Marketplace.deploy()
+  await marketplace.deployed()
+
+  console.log(`Deployed Marketplace Contract at: ${marketplace.address}\n`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
